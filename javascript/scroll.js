fe55bd1d0f9058fd0326d8d24766143619130442
Abhay Parashar <52857736+Abhayparashar31@users.noreply.github.com>
2020-08-20 14:50:12 +0530

    Update scroll.js

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -1,1 +1,13 @@
 
+function Scroll(){
+    var nav = document.getElementById("nav");
+    var pos = window.pageYOffset;
+    if(pos > 600){
+        nav.style.backgroundColor="#dc3545";
+        nav.style.height="70px";
+    }
+    else{
+        nav.style.backgroundColor="";
+    }
+}
+window.addEventListener("scroll",Scroll);
